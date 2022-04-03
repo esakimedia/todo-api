11dbb84f6e7e17d36c13144d39d3a2bdd7f720e0
esakimedia <apachecordovax@gmail.com>
2022-04-03 09:33:01 +0200

    fetch single list

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -13,4 +13,11 @@ class TodoListController extends Controller
 
         return response($lists);
     }
+
+    public function show($id)
+    {
+        $list = TodoList::find($id);
+
+        return response($list);
+    }
 }
--- tests/Feature/TodoListTest.php
+++ tests/Feature/TodoListTest.php
@@ -24,4 +24,18 @@ class TodoListTest extends TestCase
         $this->assertEquals(1, count($response->json()));
         $this->assertEquals('my list', $response->json()[0]['name']);
     }
+
+    public function test_fetch_single_todo_list()
+    {
+        // preparation
+        $list = TodoList::factory()->create();
+
+        // action
+        $response = $this->getJson(route('todo-list.show', $list->id))
+            ->assertOk()
+            ->json();
+
+        // assertion
+        $this->assertEquals($response['name'], $list->name);
+    }
 }
