7cced1859e13048832f62a3b26a5fbcdac1789ad
esakimedia <apachecordovax@gmail.com>
2022-04-03 08:24:05 +0200

    fetch from database

--- tests/Feature/TodoListTest.php
+++ tests/Feature/TodoListTest.php
@@ -2,15 +2,19 @@
 
 namespace Tests\Feature;
 
+use App\Models\TodoList;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TodoListTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_index_todo_list()
     {
         // preparation / prepare
+        TodoList::create(['name' => 'my list']);
 
         // action /perform
         $response = $this->getJson(route('todo-list.index'));
