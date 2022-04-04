8b41be7145a13cbdf4d0d41401dc741bd7a8d238
esakimedia <apachecordovax@gmail.com>
2022-04-04 10:10:30 +0200

    auth user login

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
@@ -13,3 +14,4 @@ Route::apiResource('todo-list.task', TaskController::class)
 
 
 Route::post('/register', RegisterController::class)->name('user.register');
+Route::post('/login', LoginController::class)->name('user.login');
